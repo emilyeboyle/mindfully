6fe742a6b84fc6017a418e52f5c600ff1b1b3809
sarareich <reichsara96@gmail.com>
2019-02-12 14:43:08 -0500

    constant files for emotions & their definitions

---
+++ src/constants/EmotionDefinitions.js
@@ -0,0 +1,44 @@
+export default {
+  joyful: {
+    compassionate: 'Happy that you were able to help someone else',
+    peaceful: 'Feeling calm and at ease',
+    proud: 'Happy about something you were able to do well',
+    excited: 'High energy happiness',
+    amused: 'Enjoying something entertaining',
+    relieved: 'Happy that something bad or difficult is over',
+    wonder: 'Excited by something surprising or beautiful',
+    ecstatic: 'Overpowering happiness'
+  },
+  angry: {
+    annoyed: 'Angry because of an inconvenience',
+    frustrated: 'Angry that you can’t do something you should be able to do',
+    furious: 'Strong anger that yfou can’t control',
+    exasperated: 'Extremely irritated and frustrated',
+    argumentative: 'Expressing disagreement in an angry way',
+    vengeful: 'Desire to punish someone who hurt you'
+  },
+  afraid: {
+    nervous: 'Worried about what might happen',
+    dread: 'Great fear of expected danger',
+    panicked: 'Sudden fear that you can’t control',
+    terrified: 'Very strong and overpowering fear',
+    anxious: 'Fear of expected danger and being unable to handle that danger',
+    desperate: 'Having lost hope'
+  },
+  disgust: {
+    dislike: 'Not liking or enjoying something',
+    repulsed: 'Thinking something is horrible and disgusting',
+    distaste: 'A mild unenjoyment towards something',
+    loathing: 'Intense dislike and disgust',
+    aversion: 'Unreasonable desire to avoid something due to strong dislike',
+    revulsed: 'Extreme disgust, can cause you to draw away in reaction'
+  },
+  sad: {
+    disappointed: 'Sad that something didn’t turn out the way you wanted',
+    discouraged: 'Sad that you didn’t succeed, believing you can’t do something you want to do',
+    miserable: 'Extreme unhappiness',
+    sorrowful: 'Deep sadness, maybe caused by a loss',
+    hopeless: 'Feeling that nothing good will happen',
+    melancholy: 'A deep sadness of the spirit, sometimes the cause is not obvious'
+  }
+}
