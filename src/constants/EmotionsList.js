724942ed5d69e65fccfe63730714d3482f36945a
sarareich <reichsara96@gmail.com>
2019-02-13 14:40:15 -0500

    min and max for each emotion

--- src/constants/EmotionsList.js
+++ src/constants/EmotionsList.js
@@ -1,16 +1,77 @@
 export default {
   level1: {
-    joyful: ['compassionate', 'peaceful', 'proud', 'excited'],
-    angry: ['annoyed', 'frustrated', 'furious'],
-    afraid: ['nervous', 'dread', 'panicked', 'terrified'],
-    disgust: ['dislike', 'repulsed', 'distate', 'loathing'],
-    sad: ['disappointed', 'discouraged', 'miserable', 'sorrowful']
+    joyful: [
+      {emotion: 'compassionate', min: 30, max: 70},
+      {emotion: 'peaceful', min: 0, max: 100},
+      {emotion: 'proud', min: 30, max: 90},
+      {emotion: 'excited', min: 50, max: 100}
+    ],
+    angry: [
+      {emotion: 'annoyed', min: 0, max: 20},
+      {emotion: 'frustrated', min: 0, max: 100},
+      {emotion: 'furious', min: 90, max: 100}
+    ],
+    afraid: [
+      {emotion: 'nervous', min: 0, max: 70},
+      {emotion: 'dread', min: 50, max: 100},
+      {emotion: 'panicked', min: 90, max: 100},
+      {emotion: 'terrified', min: 90, max: 100}
+    ],
+    disgust: [
+      {emotion: 'dislike', min: 0, max: 50},
+      {emotion: 'repulsed', min: 10, max: 90},
+      {emotion: 'distate', min: 10, max: 70},
+      {emotion: 'loathing', min: 80, max: 100}
+    ],
+    sad: [
+      {emotion: 'disappointed', min: 0, max: 50},
+      {emotion: 'discouraged', min: 10, max: 80},
+      {emotion: 'miserable', min: 50, max: 100},
+      {emotion: 'sorrowful', min: 60, max: 100}
+    ]
   },
   level2: {
-    joyful: ['compassionate', 'peaceful', 'proud', 'excited', 'amused', 'relieved', 'wonder', 'ecstatic'],
-    angry: ['annoyed', 'frustrated', 'furious', 'exasperated', 'argumentative', 'vengeful'],
-    afraid: ['nervous', 'dread', 'panicked', 'terrified', 'anxious', 'desperate'],
-    disgust: ['dislike', 'repulsed', 'distate', 'loathing', 'aversion', 'revulsed'],
-    sad: ['disappointed', 'discouraged', 'miserable', 'sorrowful', 'hopeless', 'melancholy']
+    joyful: [
+      {emotion: 'compassionate', min: 30, max: 70},
+      {emotion: 'peaceful', min: 0, max: 100},
+      {emotion: 'proud', min: 30, max: 90},
+      {emotion: 'excited', min: 50, max: 100},
+      {emotion: 'amused', min: 0, max: 80},
+      {emotion: 'relieved', min: 10, max: 100},
+      {emotion: 'wonder', min: 40, max: 100},
+      {emotion: 'ecstatic', min: 80, max: 100}
+    ],
+    angry: [
+      {emotion: 'annoyed', min: 0, max: 20},
+      {emotion: 'frustrated', min: 0, max: 100},
+      {emotion: 'furious', min: 90, max: 100},
+      {emotion: 'exasperated', min: 30, max: 70},
+      {emotion: 'argumentative', min: 10, max: 100},
+      {emotion: 'vengeful', min: 40, max: 100}
+    ],
+    afraid: [
+      {emotion: 'nervous', min: 0, max: 70},
+      {emotion: 'dread', min: 50, max: 100},
+      {emotion: 'panicked', min: 90, max: 100},
+      {emotion: 'terrified', min: 90, max: 100},
+      {emotion: 'anxious', min: 0, max: 100},
+      {emotion: 'desperate', min: 80, max: 100}
+    ],
+    disgust: [
+      {emotion: 'dislike', min: 0, max: 50},
+      {emotion: 'repulsed', min: 10, max: 90},
+      {emotion: 'distate', min: 10, max: 70},
+      {emotion: 'loathing', min: 80, max: 100},
+      {emotion: 'aversion', min: 10, max: 70},
+      {emotion: 'revulsed', min: 70, max: 100}
+    ],
+    sad: [
+      {emotion: 'disappointed', min: 0, max: 50},
+      {emotion: 'discouraged', min: 10, max: 80},
+      {emotion: 'miserable', min: 50, max: 100},
+      {emotion: 'sorrowful', min: 60, max: 100},
+      {emotion: 'hopeless', min: 50, max: 100},
+      {emotion: 'melancholy', min: 10, max: 80}
+    ]
   }
 }
