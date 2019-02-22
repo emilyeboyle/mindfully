5494b2dfb1b2faca4bb04a44bee67cdc5f328990
sarareich <reichsara96@gmail.com>
2019-02-22 11:19:31 -0500

    meditation bubble with animation

---
+++ src/pages/Meditation.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import MeditationBubble from "../Components/MeditationBubble";
+
+class Meditation extends Component {
+  render() {
+    return (
+      <div>
+        <MeditationBubble/>
+      </div>
+    );
+  }
+}
+
+export default(Meditation);
