eb0f17df2612236f5105f1576fdb599d2b13ac7b
sarareich <reichsara96@gmail.com>
2019-03-22 14:58:22 -0400

    progress

--- src/MusicActivity/MusicActivity.js
+++ src/MusicActivity/MusicActivity.js
@@ -4,13 +4,12 @@ import Button from '../Components/Button';
 import Notes from './Notes';
 import Tone from 'tone';
 
-
 const noteNames = Notes['notes'];
-let music = new Tone.Synth().toMaster();
+let music = new Tone.PolySynth(8, Tone.Synth).toMaster();
 const initialCellState = {
   triggered: false,
   activated: false,
-  note: "C4"
+  note: ""
 };
 const initialSequence = [
   new Array(14).fill(initialCellState),
@@ -33,14 +32,29 @@ class MusicActivity extends Component {
       currentStep: 0,
       sequence: initialSequence
     };
+
+    this.musicLoop = new Tone.Sequence((time, x) => {
+      const sequenceCopy = [...this.state.sequence];
+      for (let i = 0; i < noteNames.length; i++) {
+        const {triggered, activated, note} = sequenceCopy[x][i];
+        sequenceCopy[x][i] = {activated, triggered: x === i, note};
+        if (activated) {
+          music.triggerAttackRelease(note, '8n', time);
+        }
+        this.setState({sequence: sequenceCopy, currentStep: i});
+        console.log(this.state.currentStep);
+      }
+    }, [...new Array(8)].map((v, i) => i), '8n');
+
+    {/*this.musicLoop.loop = true;*/}
+    this.musicLoop.start();
   }
 
   handleClick(evt) {
     this.setState({
       playing: !this.state.playing
     });
-
-    console.log(this.state.playing);
+    Tone.Transport.start();
   }
 
   toggleStep(sound, step) {
@@ -52,12 +66,19 @@ class MusicActivity extends Component {
     }
 
     sequenceCopy[sound][step] = {
-      triggered: !triggered,
+      triggered,
       activated: !activated,
       note: noteNames[step]
     }
 
     this.setState({sequence: sequenceCopy});
+    console.log(this.state.sequence)
+  }
+
+  startLoop() {
+    if (this.state.playing) {
+
+    }
   }
 
   render() {
